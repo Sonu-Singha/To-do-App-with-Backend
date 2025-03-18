985364a63fa4e2a5e287e2f2cb02d7103b89c58b
Sonu Singha <deyk64032@gmail.com>
2025-03-19 00:07:16 +0530

    Fuull project development completed

--- public/script.js
+++ public/script.js
@@ -1,3 +1,20 @@
 // Code starts from here
 
-console.log("hey")
+
+
+// Getting HTML Elems
+
+let Note_Cards = document.body.querySelectorAll(".note-card");
+
+
+
+
+// Adding Event Listener
+
+Note_Cards.forEach(cards => {
+    cards.addEventListener("click", function () {
+        const noteID = this.dataset.noteId
+        window.location.href = `/view-note/${noteID}`
+        console.log("Done")
+    })
+});
--- server.js
+++ server.js
@@ -18,7 +18,8 @@ import { fileURLToPath } from 'url'; //fileURLToPath module
 
 // Importing DB
 
-import Note from "./Notes-Models/note.model.js"; 
+import noteSchema from "./Notes-Models/note.model.js";
+import { console } from 'inspector';
 
 
 
@@ -41,6 +42,7 @@ const dirname = path.dirname(filename);
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -55,21 +57,120 @@ app.set('view engine', 'ejs');
 
 
 
-// Creating Routes
+// ------------------------------------------------------Creating GET Routes
 
-app.get('/', (req, res) => {
-    res.render('index');
+// Home Page Route
+
+app.get('/', async (req, res) => {
+    const fewNotes = await noteSchema.find();
+    res.render('index', { fewNotes });
 });
 
-app.get("/all-notes", function (req, res) {
-    res.render("all-notes");
+
+
+
+
+// All Notes Route
+
+app.get("/all-notes", async function (req, res) {
+    const fewNotes = await noteSchema.find();
+    res.render("all-notes", { fewNotes });
 
 })
 
-app.post("/create-note", function (req, res) {
-    res.render("create-note")
+
+
+
+
+
+
+
+// -----------------------------------------------------Creating POST routes
+
+
+// Create Note route
+
+app.post("/create-note-page", async function (req, res) {
+    const { title, description } = req.body;
+    const noteTitle = await noteSchema.create({
+        title: title,
+        description: description
+    })
+
+    let currentID = noteTitle._id;
+    res.redirect(`/create-note/${currentID}`)
+})
+
+
+
+
+
+// Create Note route
+
+app.get(`/create-note/:id`, async function (req, res) {
+    let note = await noteSchema.findById(req.params.id)
+    res.render("create-note", { note })
+})
+
+app.post(`/create-note/:id`, async function (req, res) {
+    const { title, description } = req.body;
+    await noteSchema.findByIdAndUpdate(req.params.id,
+        {
+            title: title,
+            description: description
+        }
+    )
+
+    res.redirect("/")
 
 })
+
+
+
+// View Note Route
+
+app.get("/view-note/:id", async function (req, res) {
+    const note = await noteSchema.findById(req.params.id)
+    res.render("view-note", { note })
+})
+
+
+
+
+
+
+
+
+
+
+
+// Note Actions Route
+
+// Edit Note
+
+app.get("/create-note/:id", async function (req, res) {
+    let note = await noteSchema.findById(req.params.id)
+
+    res.render(`create-note`, { note });
+
+})
+
+
+
+
+// Delete Note
+
+app.post("/delete-note/:id", async function (req, res) {
+
+    await noteSchema.findByIdAndDelete(req.params.id);
+    res.redirect("/all-notes")
+
+
+})
+
+
+
+
 
 
 
